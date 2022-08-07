3742832309f1f0a5453379e3067f920a31f2d4d4
Hosein <hossein@linquip.com>
2022-08-07 21:10:50 +0430

    add Today component

---
+++ src/components/Today/index.tsx
@@ -0,0 +1,21 @@
+import "./style.scss";
+
+const Today = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam ad incidunt
+      inventore hic maxime quisquam aut corporis ipsa autem dolorem ab, dolor
+      asperiores, facere voluptatum obcaecati nobis in! Debitis laborum
+      suscipit, modi nihil sequi, cumque ipsam unde ut dolor quod possimus
+      aliquid iste corrupti, labore reiciendis distinctio est et. Odit
+      accusantium, ab recusandae hic accusamus voluptate. Eligendi deleniti
+      similique aperiam doloremque mollitia atque sunt modi. Quibusdam
+      architecto cum numquam pariatur tenetur eos? Expedita veniam dignissimos
+      itaque eos voluptatibus explicabo minus aut id laboriosam quia, laudantium
+      nulla reprehenderit soluta! A, natus. Laboriosam debitis molestias est
+      vitae. Neque eveniet aliquid laudantium fugiat!
+    </div>
+  );
+};
+
+export default Today;
