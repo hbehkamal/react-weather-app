664cd8f6ec06ce88f2efa14b4acaba448aa64151
Hosein <hossein@linquip.com>
2022-08-08 11:26:31 +0430

    Add search city by name API and use it

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,17 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+export const getCity = createApi({
+  reducerPath: "city",
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.REACT_APP_CITY_API_URL,
+  }),
+  endpoints: (builder) => ({
+    getCityByName: builder.query({
+      query: (name) => `json?input=${name}`,
+    }),
+  }),
+});
+
 export const getWather = createApi({
   reducerPath: "weather",
   baseQuery: fetchBaseQuery({
@@ -13,3 +25,4 @@ export const getWather = createApi({
 });
 
 export const { useGetWeatherByNameQuery } = getWather;
+export const { useGetCityByNameQuery } = getCity;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
-import { getWather } from "api";
+import { getWather, getCity } from "api";
 
 export const store = configureStore({
   reducer: {
     [getWather.reducerPath]: getWather.reducer,
+    [getCity.reducerPath]: getCity.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(getWather.middleware),
 });
 
 setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
