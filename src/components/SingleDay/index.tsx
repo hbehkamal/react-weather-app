54998a1768bf88f2a8602be01f313656f14acb36
Hosein <hossein@linquip.com>
2023-02-17 19:32:43 +0330

    Show hourly conditions

--- src/components/SingleDay/index.tsx
+++ src/components/SingleDay/index.tsx
@@ -1,31 +1,53 @@
 import { FC } from "react";
 import { Condition } from "types";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 import "./style.scss";
+import SingleHour from "components/SingleHour";
 interface IProps {
   weather: Condition;
   isToday?: boolean;
 }
 
 const SingleDay: FC<IProps> = ({ weather, isToday = false }) => {
-  const { temp, icon, conditions } = weather;
-  console.log("weather: ", weather);
+  const { temp, icon, conditions, hours } = weather;
+  console.log("weather: ", isToday, weather);
 
   return (
     <div>
       {isToday ? (
         <div className="summary-today">
-          <img src={`img/${icon}.png`} alt={conditions} />
+          <img
+            style={{ width: 154 }}
+            src={`img/${icon}.png`}
+            alt={conditions}
+          />
           <h2>{temp} C°</h2>
           <h3>{conditions}</h3>
         </div>
       ) : (
         <div className="summary-tomorrow">
-          <img src={`img/${icon}.png`} alt={conditions} />
+          <img
+            style={{ width: 154 }}
+            src={`img/${icon}.png`}
+            alt={conditions}
+          />
           <h2>{temp} C°</h2>
           <h3>{conditions}</h3>
         </div>
       )}
+      <Swiper
+        direction="vertical"
+        slidesPerView={6}
+        style={{ height: "50vw", paddingTop: 10 }}
+      >
+        {!!hours.length &&
+          hours.map(({ datetime, temp }) => (
+            <SwiperSlide>
+              <SingleHour datetime={datetime} temp={temp} />
+            </SwiperSlide>
+          ))}
+      </Swiper>
     </div>
   );
 };
