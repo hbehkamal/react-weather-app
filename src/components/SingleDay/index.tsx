6a75c9cacd846d30b2fc51f6d826fabce73a45ef
Hosein <hossein@linquip.com>
2023-02-24 22:41:14 +0330

    Show current time

--- src/components/SingleDay/index.tsx
+++ src/components/SingleDay/index.tsx
@@ -24,14 +24,23 @@ const SingleDay: FC<IProps> = ({ weather, isToday = false, city }) => {
     uvindex,
     visibility,
   } = weather;
-
+  const now = new Intl.DateTimeFormat(undefined, {
+    weekday: "long",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: false,
+  }).format(new Date());
   return (
     <div className="w-full flex flex-col h-full justify-around">
       <div className="flex flex-col items-center justify-between pt-4">
-        <div className="flex px-2 mb-16 -mt-8">
+        <span className="flex flex-col items-start w-full px-6 -mt-5">
+          <span>{now}</span>
+          <span>Max: x | Min: y</span>
+        </span>
+        <div className="flex px-2 mb-16">
           <div className="flex flex-col items-start mr-6">
-            <span>Now</span>
-            <span>Max: x | Min: y</span>
             <h2 className="text-6xl">{temp} C°</h2>
             <span>Feels like {feelslike} C°</span>
           </div>
